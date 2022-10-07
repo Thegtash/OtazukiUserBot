fe0db2e7c95341975faaf63f56da0234312f0f5c
« ͸ Tʜᴇ ɢᴛᴀsʜ ۩ <114175128+Thegtash@users.noreply.github.com>
2022-10-07 20:37:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 LOG_GROUP_ID = -1001621736306
-OWNER_ID = [5629305049]
+OWNER_ID = [1985665341]
 HANDLER = ["~", ".", "!"]
-ALIVE_TEXT = """ **~  𝑁𝑒𝑘𝑜 𝑆𝑦𝑠𝑡𝑒𝑚:**
+ALIVE_TEXT = """ **~ 𝐎𝐭𝐚𝐳𝐮𝐤𝐢 𝐔𝐬𝐞𝐫𝐛𝐨𝐭:**
 ━━━━━━━━━━━━━━━━━━━
 
 ❥ **𝑂𝑊𝑁𝐸𝑅**: {}
@@ -9,8 +9,4 @@ ALIVE_TEXT = """ **~  𝑁𝑒𝑘𝑜 𝑆𝑦𝑠𝑡𝑒𝑚:**
 ❥ **𝑃𝑂𝑁𝐺 𝑀𝑠:** {}
 ❥ **𝑈𝑃𝑇𝐼𝑀𝐸:** {}
 
-━━━━━━━━━━━━━━━━━━━
-**Meet Me Here🙈 @Besties_XD ✨🥀**
-"""
-
-NEKO = "https://telegra.ph/file/ab6b1f13355fc773f4d31.jpg"
+NEKO = "https://telegra.ph/file/94ce99e878ca951f80c9b.jpg"
